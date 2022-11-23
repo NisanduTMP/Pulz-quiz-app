27e4db6d223c6b2c979bc6c0f2bd712f4dd36488
NisanduAthsara <nisandunox@gmail.com>
2022-11-23 14:49:47 +0530

    Add tic and principal msg section

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -2,12 +2,16 @@ import React from "react";
 import Navbar from "../components/Navbar";
 import About from "../components/About";
 import Footer from "../components/Footer";
+import PrincipalMessage from "../components/PrincipalMessage";
+import TicMessage from "../components/TicMessage";
 
 export default function Home(){
     return (
         <div>
             <Navbar/>
             <About/>
+            <PrincipalMessage/>
+            <TicMessage/>
             <Footer/>
         </div>
     )
